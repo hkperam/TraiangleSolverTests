a16a69b0fc584a6370fc27e374bb26252df60099
Harikrishna <hperam9167@conestogac.on.ca>
2025-02-21 00:34:19 -0500

    Initial commit - Added TriangleSolvertesting project

---
+++ EquilateralUnitTest/Test1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+
+namespace EquilateralUnitTest
+{
+    [TestFixture]
+    public class Test1
+    {
+        [Test]
+        public void IsEquilateral()
+        {
+            int firstSide=5, secondSide=5, thirdSide=5;
+            String expected= "Equilateral triangle"; 
+            string actual=Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+            Assert.AreEqual(actual,expected);
+
+        }
+    }
+}
